68888b40cfd03b32e73d688d1527f0281a2ab9e6
Christopher <chrisobrien416@gmail.com>
2020-11-28 17:22:52 -0500

    deploying

--- src/db.js
+++ src/db.js
@@ -1,10 +1,11 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    user: 'chris',
-    host: 'ec2-34-237-166-54.compute-1.amazonaws.com',
+    user: 'zaqefocjolnifq',
+    password: '13869e6cd054823ee2f9ef782038bf14e7827bcf34c1c92b789c8ad464ccffda',
+    host: 'ec2-54-82-208-124.compute-1.amazonaws.com',
     port: 5432,
-    database: "d3mhf43jk81eu3"
+    database: "d28avgd619b22i"
 })
 
 module.exports = pool;
