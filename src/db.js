db75c8fb40cf58446101a93e941a920afc6c70c3
Christopher <chrisobrien416@gmail.com>
2020-11-30 17:37:16 -0500

    more

--- src/db.js
+++ src/db.js
@@ -1,12 +1,13 @@
 const Pool = require('pg').Pool
+const {USER, PASSWORD, DATABASE, DBPORT, HOST} = require('./config')
 require('dotenv').config()
 
 const pool = new Pool({
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    host: process.env.HOST,
-    port: process.env.DBPORT,
-    database: process.env.DATABASE
+    user: USER,
+    password: PASSWORD,
+    host: HOST,
+    port: DBPORT,
+    database: DATABASE
 })
 
 // const pool = new Pool({
