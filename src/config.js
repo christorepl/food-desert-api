4597462b23680909babfacaa3b56041b0c985e3b
Christopher <chrisobrien416@gmail.com>
2020-11-30 17:04:06 -0500

    fixing

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8001,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: 'postgres://pqyjfeimvnedzh:98fbe625d5cc10466c7e841887edcfb81e592779fc372cb7dc80b7ceb307c404@ec2-34-237-166-54.compute-1.amazonaws.com:5432/d3mhf43jk81eu3' || 'postgresql://chris@localhost/food-desert',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://chris@localhost/food-desert',
     RAPID_API_KEY: process.env.RAPID_API_KEY || process.env.RAPID_API_KEY,
     CLIENT_ORIGIN: 'https://food-desert.vercel.app/',
     USER: process.env.USER,
