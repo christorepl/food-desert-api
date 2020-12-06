fc504b3e51bb2abe15272ae05391cdc08e18306d
Christopher <chrisobrien416@gmail.com>
2020-12-05 22:45:16 -0500

    mvp

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 module.exports = {
     PORT: process.env.PORT || 8001,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: 'postgresql://chris@localhost/food-desert',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://chris@localhost/food-desert',
     RAPID_API_KEY: process.env.RAPID_API_KEY || process.env.RAPID_API_KEY,
-    CLIENT_ORIGIN: '*',
+    CLIENT_ORIGIN: 'https://food-desert.vercel.app' || 'home',
     USER: process.env.USER,
     PASSWORD: process.env.PASSWORD,
     DATABASE: process.env.DATABASE,
