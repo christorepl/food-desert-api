353f4fbdd3b57c52215bcac3c6f1921238b7f0ae
Christopher <chrisobrien416@gmail.com>
2020-12-08 00:42:22 -0500

    more

--- src/app.js
+++ src/app.js
@@ -50,7 +50,45 @@ app.get('/api/state/all', async (req, res, next) => {
   })
  StatesService.getAllStates(knexInstance)
     .then(states => {
-      res.json(states)
+      for (let i = 0; i < states.length; i++) {
+        //calculate covid rate and covid fatality rate and enter it into the respective state object
+        states[i].covid_rate=(new Intl.NumberFormat().format((states[i].covid_infections/states[i].pop)*100))
+        states[i].covid_fatality_rate=(new Intl.NumberFormat().format((states[i].covid_deaths/states[i].covid_infections)*100))
+        }
+        //these next few .sort's allow me to enter rankings for covid stats [infections, deaths, infection rate and fatality rate]
+        states.sort((a,b) => {
+          return a.covid_rate - b.covid_rate
+        })
+        const sortCovidRateStates = states.reverse()
+        for (let i = 0; i < sortCovidRateStates.length; i++) {
+          sortCovidRateStates[i].ranking_covid_rate=(i + 1)
+        }
+        sortCovidRateStates.sort((a, b) => {
+          return a.covid_fatality_rate - b.covid_fatality_rate
+        })
+        const sortCovidFatalityRateStates = sortCovidRateStates.reverse()
+        for (let i = 0; i < sortCovidFatalityRateStates.length; i++) {
+          sortCovidFatalityRateStates[i].ranking_covid_fatality_rate=(i + 1)
+        }
+        sortCovidFatalityRateStates.sort((a, b) => {
+          return a.covid_infections - b.covid_infections
+        })
+        const sortCovidInfectionsStates = sortCovidFatalityRateStates.reverse()
+        for (let i = 0; i < sortCovidInfectionsStates.length; i++) {
+          sortCovidInfectionsStates[i].ranking_covid_infections=(i + 1)
+        }
+        sortCovidInfectionsStates.sort((a, b) => {
+          return a.covid_deaths - b.covid_deaths
+        })
+        const sortCovidDeathsStates = sortCovidInfectionsStates.reverse()
+        for (let i = 0; i< sortCovidDeathsStates.length; i++) {
+          sortCovidDeathsStates[i].ranking_covid_deaths=(i + 1)
+        }
+        sortCovidDeathsStates.sort((a, b) => {
+          return a.id - b.id
+        })
+        let organizedStates = sortCovidDeathsStates
+      res.json(organizedStates)
     })
     .catch(next)
 })
@@ -65,6 +103,8 @@ app.get('/api/state/search', async (req, res, next) => {
   for (let i = 0; i < fips.length; i++){
    await StatesService.getByFips(knexInstance, fips[i])
     .then(state => {
+      state[0].covid_rate=(new Intl.NumberFormat().format((state[0].covid_infections/state[0].pop)*100))
+      state[0].covid_fatality_rate=(new Intl.NumberFormat().format((state[0].covid_deaths/state[0].covid_infections)*100))
       if (!state) {
         return res.status(404).json({
           error : { message: `A US state with fips code ${fips} does not exist`}
--- src/db.js
+++ src/db.js
@@ -25,4 +25,5 @@ const pool = new Pool({
 //     port: 5432,
 //     database: 'food-desert-test'
 // })
+
 module.exports = pool;
