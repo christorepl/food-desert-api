9937dd0e772bab514d5824c187729ccfb87f1a5c
Christopher <chrisobrien416@gmail.com>
2020-12-11 16:42:13 -0500

    done

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
     PORT: process.env.PORT || 8001,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://chris@localhost/food-desert',
-    CLIENT_ORIGIN: 'https://food-desert.vercel.app' || 'home',
+    CLIENT_ORIGIN: 'https://food-justice-now.vercel.app' || 'home',
     RAPID_API_KEY: process.env.RAPID_API_KEY || process.env.RAPID_API_KEY,
     USER: process.env.USER,
     jwtSecret: process.env.jwtSecret,
@@ -13,5 +13,5 @@ module.exports = {
 }
 
     // DATABASE_URL: process.env.DATABASE_URL || 'postgresql://chris@localhost/food-desert',
-    // CLIENT_ORIGIN: 'https://food-desert.vercel.app' || 'home',
+    // CLIENT_ORIGIN: 'https://food-justice-now.vercel.app' || 'home',
 
