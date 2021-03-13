8858d077ad900b5bdc35a62155eb425a47f21f29
Christopher <chrisobrien416@gmail.com>
2021-03-13 13:43:47 -0500

    fixes

--- src/config.js
+++ src/config.js
@@ -1,18 +1,18 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
-    PORT: process.env.PORT || 8001,
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: 'postgresql://chris@localhost/food-desert',
-    CLIENT_ORIGIN: '*',
-    RAPID_API_KEY: process.env.RAPID_API_KEY,
-    USER: process.env.USER,
-    jwtSecret: process.env.jwtSecret,
-    PASSWORD: process.env.PASSWORD,
-    DATABASE: process.env.DATABASE,
-    DBPORT: process.env.DBPORT,
-    HOST: process.env.HOST
-}
+  PORT: process.env.PORT || 8001,
+  NODE_ENV: process.env.NODE_ENV || "development",
+  DATABASE_URL: process.env.DATABASE_URL,
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN,
+  RAPID_API_KEY: process.env.RAPID_API_KEY,
+  USER: process.env.USER,
+  jwtSecret: process.env.jwtSecret,
+  PASSWORD: process.env.PASSWORD,
+  DATABASE: process.env.DATABASE,
+  DBPORT: process.env.DBPORT,
+  HOST: process.env.HOST,
+};
 
 // DATABASE_URL: process.env.DATABASE_URL || 'postgresql://chris@localhost/food-desert',
 // CLIENT_ORIGIN: 'https://food-justice-now.vercel.app' || 'home',
